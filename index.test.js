c7a4c01eb88fea30be374a27ae18a3bcaa6103d2
Dahee Cha <daheecha0612@gmail.com>
2021-04-20 20:58:53 +0900

    Refactor: Parameterized Test

--- index.test.js
+++ index.test.js
@@ -1,9 +1,12 @@
 const { test, expect } = require("@jest/globals");
 const sut = require("./index"); // sut = System Under Test
 
-test('sut correctly work', () => {
-	for (const source of ['hello  world', 'hello   world', 'hello    world']) {
-		const actual = sut(source);
-		expect(actual).toBe("hello world");
-	}
+test.each`
+  source             | expected
+  ${"hello world"}   | ${"hello world"}
+  ${"hello   world"} | ${"hello world"}
+  ${"hello  world"}  | ${"hello world"}
+`("sut transfrom $source to $expected", ({ source, expected }) => {
+  const actual = sut(source);
+  expect(actual).toBe(expected);
 });
